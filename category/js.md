97253ebb95be692b65efcf9d1d0d8049a3d3fcd6
xiawen <xiawen@xiawendeMacBook-Pro.local>
2020-07-13 15:24:13 +0800

    fix: vue

--- category/js.md
+++ category/js.md
@@ -1,5 +1,3 @@
 - [bind 实现](https://github.com/xiaotangdou/daily-question-js/issues/1)
-
 - [把同步回调函数包装成变异步函数，fn(arg, cb) >>> fn(arg).then(func).catch(func)](https://github.com/xiaotangdou/daily-question-js/issues/2)
-
 - [JavaScript 中的 null 是一个对象吗?](https://github.com/xiaotangdou/daily-question-js/issues/3)
--- practice-env/js-env/src/index.js
+++ practice-env/js-env/src/index.js
@@ -18,3 +18,34 @@
 // promisify((payload, cb) => cb(payload))('test').then(res => console.log(res)).catch(err => console.log(err))
 
 console.log("------ 我是分割线 ------");
+
+let obj = {
+  a: 1,
+  test: () => {
+    console.log(this);
+    console.log(this.a);
+  },
+};
+
+obj.test(); // 1
+
+const test = obj.test;
+test(); // undefined
+
+test.bind(obj);
+test(); // undefined
+
+test.bind(obj)(); // 1
+
+test.call(obj); // 1
+
+console.log("------ 我是分割线 ------");
+
+// 实现bind函数
+function bindContext(fn, context) {
+  return function () {
+    fn.apply(context, arguments);
+  };
+}
+
+bindContext(test, obj)();
